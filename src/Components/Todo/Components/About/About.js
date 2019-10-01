4b50ecdef46616210fbd8b3f44f888678d8e7be4
Lucia Garajová <lucia.garajova.upa@gmail.com>
2019-10-01 12:37:22 +0200

    text_update

--- src/Components/Todo/Components/About/About.js
+++ src/Components/Todo/Components/About/About.js
@@ -13,11 +13,11 @@ class About extends Component {
                 </div>
                 <div className="as_intro">
                     <p className="as_top">
-                        This project is a simple To Do list that was made as a practical exercise to learn React.
+                        This project is a simple 'To Do List' that was made as a practical exercise to learn React.
                     </p>
                     <p className="as_middle">
-                        The To Do list uses local storage of the client's browser, so al the data stays in the user's local storage until the storage is cleared.
-                        The other features are "add", "delete" and "edit" the tasks.
+                        The 'To Do List' uses local storage of the client's browser, so all the data stays in the user's local storage until the storage is cleared.
+                        The other features are 'add', 'delete' and 'edit' the tasks.
                         By clicking on the task's title, the details of the specific task, that are not originally visible, are revealed.
                     </p>
                 </div>
